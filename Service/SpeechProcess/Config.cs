8ee14629b48cf41794ac2ec6a226b1b992f85350
Drrrring <y6655533@163.com>
2022-07-02 15:00:47 +0800

    add: secret config

---
+++ Service/SpeechProcess/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.SpeechProcess;
+
+public class Config
+{
+    /*
+	 * Please do not commit the secrets to the repository.
+	 */
+
+    public const string SECRET_ID = "";
+
+    public const string SECRET_KEY = "";
+}
