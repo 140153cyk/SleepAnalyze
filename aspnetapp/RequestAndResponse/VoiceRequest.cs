7b003184c6cdbde5694e8e39031199ba4e6d672c
Drrrring <y6655533@163.com>
2022-07-06 11:42:40 +0800

    update: attributes in VoiceRequest and VoiceResponse

--- aspnetapp/RequestAndResponse/VoiceRequest.cs
+++ aspnetapp/RequestAndResponse/VoiceRequest.cs
@@ -2,10 +2,17 @@
 
 public class VoiceRequest
 {
-    public string url { get; set; }
+    public string url { get; set; } = "";
+    public string audioFormat { get; set; } = "";
 
     public VoiceRequest()
     {
         
+    }
+
+    public VoiceRequest(string url, string audioFormat)
+    {
+        this.url = url;
+        this.audioFormat = audioFormat;
     }
 }
--- aspnetapp/RequestAndResponse/VoiceResponse.cs
+++ aspnetapp/RequestAndResponse/VoiceResponse.cs
@@ -1,18 +1,25 @@
 ﻿namespace aspnetapp.RequestAndResponse;
+using Service.Wav;
 
 public class VoiceResponse
 {
     public bool talkInSleep { get; set; }
     public string speech { get; set; }
+    public double db { get; set; } = -1;
+    public VoiceKind voiceKind { get; set; } = VoiceKind.UNUSE;
+    public string url { get; set; }
 
     public VoiceResponse()
     {
         
     }
     
-    public VoiceResponse(bool talkInSleep, string speech)
+    public VoiceResponse(bool talkInSleep, string speech, double db, VoiceKind voiceKind, string url)
     {
         this.talkInSleep = talkInSleep;
         this.speech = speech;
+        this.db = db;
+        this.voiceKind = voiceKind;
+        this.url = url;
     }
 }
