f019dcb7495a23d4438539a09b1b0140daae76f1
Drrrring <y6655533@163.com>
2022-07-06 11:44:51 +0800

    update: tests of speech process

--- Tests/SpeechProcessTest.cs
+++ Tests/SpeechProcessTest.cs
@@ -15,7 +15,7 @@ public class SpeechProcessTest
         string url = "https://636c-cloud1-8gbtq97y394b518b-1311389966.tcb.qcloud.la/records/2022/07/02%2010%3A41%3A54-37850.mp3?sign=2b765254c939b245c50f4a5f08e5e764&t=1656729958";
         var s = HttpUtility.UrlPathEncode(url);
         Console.WriteLine(s);
-        var res = ASR.RecognizeSpeech(url, "1");
+        var res = ASR.RecognizeSpeech(url, "1", "m4a");
         Console.WriteLine(res);
     }
 
